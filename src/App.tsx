934b52ebe61f98fdca2de7b342376658592eefaf
abdallahleandro <abdallahleandrodev@gmail.com>
2026-08-17 15:31:28 -0300

    Instalando yup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
 
 import { Controller, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 type FormData = {
   name: string;
@@ -8,6 +10,8 @@ type FormData = {
   subject: string;
   description: string;
 };
+
+const schema = yup.object({});
 
 export default function App() {
   const { control, handleSubmit } = useForm<FormData>({
@@ -17,6 +21,7 @@ export default function App() {
       subject: "",
       description: "",
     },
+    resolver: yupResolver(schema),
   });
 
   function onSubmit(data: FormData) {
